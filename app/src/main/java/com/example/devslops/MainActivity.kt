0d6fef1c22d0ac591a8ad93c50198be3259b35d5
Elly <blackcoda56@gmail.com>
2023-09-01 12:06:21 +0300

    Tablet view

--- app/src/main/java/com/example/devslops/MainActivity.kt
+++ app/src/main/java/com/example/devslops/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.devslops
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val theLogo = findViewById<ImageView>(R.id.logo)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 20f // this makes rounded edges
+        //rounded.isCircular = true // this makes circular shape
+        theLogo.setImageDrawable(rounded)
     }
 }
